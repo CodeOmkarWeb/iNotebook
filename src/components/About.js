0d914c48c19925262c59c621ba02fedab53f4a24
Omkar <omkardmessi@gmail.com>
2022-03-04 18:54:04 +0530

    Completed Video #58

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import noteContext from '../context/notes/noteContext'
+import { useEffect } from 'react'
 export default function About() {
+    const a = useContext(noteContext)
+    useEffect(() => {
+      a.update()
+    }, [])
+    
     return (
         <>
+        {a.state.name}
+        {a.state.class}
         <h1>This is About</h1>
         </>
     )
